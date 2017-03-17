f739c34980db3859d2e0fccf33c5d1a7937f1322
Sean Perez <sean.perez29@gmail.com>
2017-03-16 23:27:37 -0600

    Initial Commit

---
+++ DevslopesTutorials/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DevslopesTutorials
+//
+//  Created by Sean Perez on 3/16/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DevslopesTutorials/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DevslopesTutorials
+//
+//  Created by Sean Perez on 3/16/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
